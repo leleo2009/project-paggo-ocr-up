cffb6a3e4dc9ea05879e6eec6eafced1506a61c4
Leonardo <148481349+leleo2009@users.noreply.github.com>
2025-04-19 01:47:59 -0300

    corrigindo o main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(parseInt(process.env.PORT ?? '3001'), '0.0.0.0');
 }
 bootstrap();
